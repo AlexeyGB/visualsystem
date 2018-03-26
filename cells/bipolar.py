e3e306570e5770ff24aff576d6f6c9628064dc23
Alexey Bauman <alxgenba@gmail.com>
2018-03-26 18:32:14 +0300

    Change word "periphery" to more proper "surround" in cells/bipolar.py

--- cells/bipolar.py
+++ cells/bipolar.py
@@ -18,30 +18,30 @@ class BipolarBinary:
             Array of references to cells related
             to the center of the receptive field
 
-        input_periphery : array-like
+        input_surround : array-like
             Array of references to cells related
-            to the periphery of the receptive field
+            to the surround of the receptive field
 
-        central_peripheral_tolerance : {'linear', 'elliptical'}
-            The dependence between the proportion of positive central
-            inputs and acceptable proportion of positive peripheral
+        center_surround_tolerance : {'linear', 'elliptical'}
+            The dependence between the proportion of positive center
+            inputs and acceptable proportion of positive surround
             inputs for cell with on-center (for cell with off-center
             conversely)
 
             - 'linear', grows linearly
 
             - 'elliptical', grows like a quoter of an ellipse with
-                center in (1, 0) and half-axis 1-central_threshold
-                and peripheral_threshold (see below)
+                center in (1, 0) and half-axis 1-center_threshold
+                and surround_threshold (see below)
 
-        central_threshold : float, [0; 1], default 0.8
+        center_threshold : float, [0; 1], default 0.8
             Minimal proportion of positive (or negative for off-center
-            cell's type) central inputs, required to be able to response
+            cell's type) center inputs, required to be able to response
             positively
 
-        peripheral_threshold : float, [0; 1], default 0.2
+        surround_threshold : float, [0; 1], default 0.2
             Maximal proportion of positive (or negative for off-center
-            cell's type) peripheral inputs, required to be able to response
+            cell's type) surround inputs, required to be able to response
             positively
 
         center_type : int, {-1, 1}, default 1
@@ -75,48 +75,48 @@ class BipolarBinary:
     """
 
     def __init__(self, position, input_center,
-                 input_periphery, central_peripheral_tolerance='linear',
-                 central_threshold=0.8, peripheral_threshold=0.2,
+                 input_surround, center_surround_tolerance='linear',
+                 center_threshold=0.8, surround_threshold=0.2,
                  center_type=1, n_iter=0):
         self.position = position
         self.n_iter = n_iter
         self._input_center = input_center
-        self._input_periphery = input_periphery
-        self._central_peripheral_tolerance = central_peripheral_tolerance
-        self._central_threshold = central_threshold
-        self._peripheral_threshold = peripheral_threshold
+        self._input_surround = input_surround
+        self._center_surround_tolerance = center_surround_tolerance
+        self._center_threshold = center_threshold
+        self._surround_threshold = surround_threshold
         self.center_type = center_type
         self._response = 0
 
     def _calculate_response(self):
-        central_in = 0
-        for central_cell in self._input_center:
-            central_in += central_cell.get_response()
+        center_in = 0
+        for center_cell in self._input_center:
+            center_in += center_cell.get_response()
 
-        peripheral_in = 0
-        for peripheral_cell in self._input_periphery:
-            peripheral_in += peripheral_cell.get_response()
+        surround_in = 0
+        for surround_cell in self._input_surround:
+            surround_in += surround_cell.get_response()
 
         if self.center_type == 1:
-            central_positive_in_share = central_in/len(self._input_center)
-            peripheral_positive_in_share = peripheral_in/len(self._input_periphery)
+            center_positive_in_share = center_in/len(self._input_center)
+            surround_positive_in_share = surround_in/len(self._input_surround)
         else:
-            central_positive_in_share = 1 - central_in/len(self._input_center)
-            peripheral_positive_in_share = 1 - peripheral_in/len(self._input_periphery)
+            center_positive_in_share = 1 - center_in/len(self._input_center)
+            surround_positive_in_share = 1 - surround_in/len(self._input_surround)
 
-        if self._central_peripheral_tolerance == 'linear':
-            if peripheral_positive_in_share <= tolerance_line(central_positive_in_share,
-                                                              self._central_threshold,
-                                                              self._peripheral_threshold
+        if self._center_surround_tolerance == 'linear':
+            if surround_positive_in_share <= tolerance_line(center_positive_in_share,
+                                                              self._center_threshold,
+                                                              self._surround_threshold
                                                               ):
                 response = 1
             else:
                 response = 0
 
-        elif self._central_peripheral_tolerance == 'elliptical':
-            if peripheral_positive_in_share <= tolerance_ellipse(central_positive_in_share,
-                                                                 self._central_threshold,
-                                                                 self._peripheral_threshold
+        elif self._center_surround_tolerance == 'elliptical':
+            if surround_positive_in_share <= tolerance_ellipse(center_positive_in_share,
+                                                                 self._center_threshold,
+                                                                 self._surround_threshold
                                                                  ):
                 response = 1
             else:
